546401a8f03715a3483093e51e6f053e38f87186
Ayushi Shah <as17726@nyu.edu>
2023-10-12 14:37:31 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,19 @@
+let font;
+
+let size = 40;
+
+let s0 = "The way a crow";
+
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
+  background(0, 191, 255); 
+}
+
+
+function preload() {
+  font = loadFont("./ostrich-sans-regular.ttf");
 }
