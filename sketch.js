710629935eb11657977ee8c48b0c6f76e7ec3aa2
Ayushi Shah <as17726@nyu.edu>
2023-10-12 15:50:34 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,18 +2,35 @@ let font;
 
 let size = 40;
 
-let s0 = "The way a crow";
+let s0 = "The way a crow"; 
+//let y = 10;  
 
 
+function preload() {
+  //font = loadFont("./ostrich-sans-regular.ttf");
+}
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  textFont("monospace");
+  textSize(24);
+  textAlign(LEFT, TOP);
+  fill(255);
+  words = s0.split(" ");
 }
 
 function draw() {
   background(0, 191, 255); 
-}
-
-
-function preload() {
-  font = loadFont("./ostrich-sans-regular.ttf");
+  let distance = height/words.length; 
+  let y = distance; 
+  for (let i = 0; i < words.length; i++) {
+    let word = words[i].toUpperCase(); 
+    //if(words.length%2 ==1) {
+     //midpoint = words.length+1/2; 
+    //}
+    //else if (word.length%2 ==0) {
+      //midpoint = words.length/2; 
+    //}
+    y = y + distance; 
+    text (word, width/2, y); 
+  } 
 }
